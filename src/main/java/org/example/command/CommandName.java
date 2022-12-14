5a223a8d8130f1bf36577dbf766f854f19d25933
Zitronenp <114046496+Zitronenp@users.noreply.github.com>
2022-12-14 11:02:32 +0500

    Change to abstract (#1)

--- src/main/java/org/example/command/CommandName.java
+++ src/main/java/org/example/command/CommandName.java
@@ -3,26 +3,42 @@ package org.example.command;
 import org.example.parsing.Parsing;
 
 public enum CommandName {
-    START("/start", "Enter /weather"),
-    HELP("/help", "Command /start to get started\n" +
-            "Command /help for help (no one will help you anymore)\n" +
-            "Command /weather get information about weather in Ekaterinburg"),
-    WEATHER("/weather", "Temperature in Ekaterinburg now is " + Parsing.parsing()),
-    UNKNOWN("", "Sorry, I don't know this command ¯\\_(ツ)_/¯");
+    START("/start") {
+        @Override
+        public String getAnswer() {
+            return "Enter /weather";
+        }
+    },
+    HELP("/help") {
+        @Override
+        public String getAnswer() {
+            return  "Command /start to get started\n" +
+                    "Command /help for help (no one will help you anymore)\n" +
+                    "Command /weather get information about weather in Ekaterinburg";
+        }
+    },
+    WEATHER("/weather" ) {
+        @Override
+        public String getAnswer() {
+            return "Temperature in Ekaterinburg now is " + Parsing.parsing();
+        }
+    },
+    UNKNOWN("") {
+        @Override
+        public String getAnswer() {
+            return "Sorry, I don't know this command ¯\\_(ツ)_/¯";
+        }
+    };
 
     private String name;
-    private String answer;
 
-    CommandName(String name, String response) {
+    CommandName(String name) {
         this.name = name;
-        this.answer = response;
     }
 
     public String getName() {
         return name;
     }
 
-    public String getAnswer() {
-        return answer;
-    }
+    public abstract String getAnswer();
 }
